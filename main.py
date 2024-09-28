072c4b25c286bc86ba1ed977a3597401ce5445e5
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2024-09-28 13:32:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,82 +6,37 @@ import random
 app = Flask(__name__)
 
 
-@app.route("/")
-def helloworld():
-    return 'Hello World'
-
-
 @app.route('/efficient-hunter-kazuma', methods=['POST'])
 def efficient_hunter_kazuma():
-    data = request.get_json()
-    data = f'{data}'
-    print(f'{data}')
-    data = data.replace("\'", "\"")
-    print(data)
-    data = json.loads(f'{data}')
-    print(data)
+    data = request.json
+    data = json.loads(data)
     results = []
     for entry in data:
-        print(entry)
         monsters = entry.get('monsters')
         efficiency = calculate_efficiency(monsters, 0, 0)
         results.append({"efficiency": efficiency})
-
-    print(results)
     return json.dumps(results)
 
 
 def calculate_efficiency(monsters, gold, stage):
-    list = []
+    if not monsters:
+        return gold
+
     if stage == 0:
-        # Prepare circle
-        if len(monsters) == 0 or len(monsters) == 1:
-            return gold
-        else:
-            # go stage 1
-            temp = monsters.copy()
-            temp.pop(0)
-
-            nextstage1 = calculate_efficiency(temp, gold - monsters[0], 1)
-            list.append(nextstage1)
-            # stay stage 0
-            temp = monsters.copy()
-            temp.pop(0)
-            staystage0 = calculate_efficiency(temp, gold, 0)
-            list.append(staystage0)
+        return max(
+            calculate_efficiency(monsters[1:], gold - monsters[0], 1),
+            calculate_efficiency(monsters[1:], gold, 0)
+        )
     elif stage == 1:
-        # Attack
-        if len(monsters) == 1:
-            return gold + monsters[0]
-        elif len(monsters) == 0:
-            return gold
-        else:
-            # now attack
-            temp = monsters.copy()
-            temp.pop(0)
-            nowattack = calculate_efficiency(temp, gold + monsters[0], 2)
-            list.append(nowattack)
-            # later attack
-            temp = monsters.copy()
-            temp.pop(0)
-            laterattack = calculate_efficiency(temp, gold, 1)
-            list.append(laterattack)
+        return max(
+            calculate_efficiency(monsters[1:], gold + monsters[0], 2),
+            calculate_efficiency(monsters[1:], gold, 1)
+        )
     elif stage == 2:
-        # Rest
-        if (len(monsters) == 0) or (len(monsters) == 1):
-            return gold
-        else:
-            temp = monsters.copy()
-            temp.pop(0)
-            return calculate_efficiency(temp, gold, 0)
+        return calculate_efficiency(monsters[1:], gold, 0)
     else:
-        # Add handling for other stages
-        pass
-
-    max_gold = max(list)
-
-    return max_gold
+        return gold
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
